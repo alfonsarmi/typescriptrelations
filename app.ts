e83639ec2a9c3ff7db463a467bd7476df4464ff9
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-03-05 08:26:24 +0100

    a

---
+++ app.ts
@@ -0,0 +1,9 @@
+/// <reference path="./circleMath.ts" />
+/// <reference path="./rectangleMath.ts" />
+
+import Circle = MyMath.Circle;
+import calculateRectangle = MyMath.Rectangle.calculateRectangle;
+
+console.log(Circle.PI); // Prints: 3.14
+console.log(Circle.calculateCircumference(10)); // Prints 31.42
+console.log(calculateRectangle(25, 25)); // Prints: 625
